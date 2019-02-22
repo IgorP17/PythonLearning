75ead27d70df6aac8615af48694aa137513a35a6
Igor <iprokhorovster@gmail.com>
2019-02-22 22:22:09 +0300

    begin

---
+++ python-osnovy-programmirovaniya/Pingvin.py
@@ -0,0 +1,13 @@
+l1 = "   _~_    "
+l2 = "  (o o)   "
+l3 = " /  V  \\  "
+l4 = "/(  _  )\\ "
+l5 = "  ^^ ^^   "
+
+n = input()
+n = int(n)
+print(l1 * n)
+print(l2 * n)
+print(l3 * n)
+print(l4 * n)
+print(l5 * n)
---
+++ python-osnovy-programmirovaniya/draft.py
@@ -0,0 +1,18 @@
+# остатки
+print(11 % 5)
+print(-11 % 5)
+print("Why not -1? Yoba magic")
+print(11 % -5)
+print(-11 % -5)
+
+# int to str
+print("int to str")
+print("2 + 3 =", str(2 + 3))
+
+# чтение данных
+print("Read data, sum")
+# a = input("a = ")
+# b = input("b = ")
+# print (a, "+", b, "=", int(a) + int(b))
+
+
