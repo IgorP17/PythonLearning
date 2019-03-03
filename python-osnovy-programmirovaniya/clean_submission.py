62abc21adc38a04f8124c7bcf32950d20db9212d
Игорь Прохоров <iprokhorov@mobicont.ru>
2019-03-03 11:15:47 +0300

    go forward week 3

--- python-osnovy-programmirovaniya/clean_submission.py
+++ python-osnovy-programmirovaniya/clean_submission.py
@@ -1,5 +1,19 @@
 import math
-s = float(input())
-r = math.floor(s)
-k = math.ceil((s - r) * 100)
-print(r, "{:02d}".format(k))
+a = float(input())
+b = float(input())
+c = float(input())
+di = b ** 2 - 4 * a * c
+if di > 0:
+    x1 = (-b + math.sqrt(di)) / (2 * a)
+    x2 = (-b - math.sqrt(di)) / (2 * a)
+    if x1 - int(x1) == 0.0:
+        x1 = int(x1)
+    if x2 - int(x2) == 0.0:
+        x2 = int(x2)
+    if x1 < x2:
+        print(x1, x2)
+    else:
+        print(x2, x1)
+elif di == 0:
+    x = -b / (2 * a)
+    print(x)
--- python-osnovy-programmirovaniya/submission_Week_03.py
+++ python-osnovy-programmirovaniya/submission_Week_03.py
@@ -27,5 +27,54 @@ import math
 # то есть действительным числом с двумя цифрами после десятичной точки.
 # Запишите в две целочисленные переменные стоимость товара
 # в виде целого числа рублей и целого числа копеек и выведите их на экран
-f = float(input())
-print(math.floor(f), int(f))
+# import math
+# s = float(input())
+# r = math.floor(s)
+# k = math.ceil((s - r) * 100)
+# print(r, "{:02d}".format(k))
+#
+# rubles = float(input())
+# kopeks = round(rubles * 100)
+# print(*divmod(kopeks, 100))
+
+# По российский правилам числа округляются до ближайшего целого числа,
+# а если дробная часть числа равна 0.5, то число округляется вверх.
+# Дано неотрицательное число x, округлите его по этим правилам.
+# from math import floor, ceil
+# x = float(input())
+# if x - floor(x) < 0.5:
+#     x = floor(x)
+# else:
+#     x = ceil(x)
+# print(x)
+
+# Процентная ставка по вкладу составляет P процентов годовых,
+# которые прибавляются к сумме вклада.
+# Вклад составляет X рублей Y копеек. Определите размер вклада через год
+# p = int(input())
+# x = int(input())
+# y = int(input())
+# money_before = 100 * x + y
+# money_after = int(money_before * (100 + p) / 100)
+# print(money_after // 100, money_after % 100)
+
+# Даны действительные коэффициенты a, b, c, при этом a != 0.
+# Решите квадратное уравнение ax²+bx+c=0 и выведите все его корни.
+# a = float(input())
+# b = float(input())
+# c = float(input())
+# di = b ** 2 - 4 * a * c
+# if di > 0:
+#     x1 = (-b + math.sqrt(di)) / (2 * a)
+#     x2 = (-b - math.sqrt(di)) / (2 * a)
+#     if x1 - int(x1) == 0.0:
+#         x1 = int(x1)
+#     if x2 - int(x2) == 0.0:
+#         x2 = int(x2)
+#     if x1 < x2:
+#         print(x1, x2)
+#     else:
+#         print(x2, x1)
+# elif di == 0:
+#     x = -b / (2 * a)
+#     print(x)
