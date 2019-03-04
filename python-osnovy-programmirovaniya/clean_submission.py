cb6aa630a3e63bbce7fd65c9f6fedd5db7fda6ec
Igor <iprokhorovster@gmail.com>
2019-03-04 21:53:33 +0300

    go forward week 4

--- python-osnovy-programmirovaniya/clean_submission.py
+++ python-osnovy-programmirovaniya/clean_submission.py
@@ -1,2 +1,11 @@
-s = input()
-print(s.replace("@", ""))
+
+
+def min4(a1, b1, c1, d1):
+    return min(a1, b1, c1, d1)
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(min4(a, b, c, d))
