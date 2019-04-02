196e6f4db60b38cb8b14712668a24c239de8876a
Igor Prokhorov <prokhorov@bpcbt.com>
2019-04-02 18:18:46 +0300

    server

---
+++ client-server/server.py
@@ -0,0 +1,34 @@
+import os
+import socket
+import threading
+import multiprocessing
+
+
+def worker(my_socket):
+    while True:
+        conn, addr = my_socket.accept()
+        print("pid", os.getpid())
+        th = threading.Thread(target=process_request, args=(conn, addr))
+        th.start()
+
+
+def process_request(conn, addr):
+    print("connected client:", addr)
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data.decode("utf8"))
+
+
+with socket.socket() as sock:
+    sock.bind(("127.0.0.1", 10001))
+    sock.listen(3)
+    workers_count = 3
+    workers_list = [multiprocessing.Process(target=worker, args=(sock,))
+                    for _ in range(workers_count)]
+    for w in workers_list:
+        w.start()
+    for w in workers_list:
+        w.join()
